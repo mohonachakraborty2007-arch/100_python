647d7a9a5fb0878776cf685ee4c442e307a8cfc5
Mohona Chakraborty <mohonachakraborty2007@gmail.com>
2026-08-21 12:28:05 +0530

    add my 9 proj

---
+++ Leap year checker.py
@@ -0,0 +1,7 @@
+#leap year checker 
+
+year= int(input("Enter your year: "))
+if year % 400 ==0 or (year % 4 == 0 and year % 100 != 0): 
+    print ("It is a leap year.")
+else:
+    print("It is not a leap year")    
