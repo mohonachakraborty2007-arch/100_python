e5f58e08684348642d9c92ae741934473f64e935
Mohona Chakraborty <mohonachakraborty2007@gmail.com>
2026-08-21 14:44:16 +0530

    add my 10 proj

---
+++ voting elegibility.py
@@ -0,0 +1,8 @@
+#votingg eligibility checker
+
+age= int(input("Enter your age:  "))
+
+if age >= 18 :
+    print("You are eligible to vote.")
+else: 
+    print("HEY, you are still a kid to vote.. ")    
