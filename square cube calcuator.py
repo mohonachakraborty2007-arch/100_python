b8790f0dc48637d0c9b3b0ec53f9edba93863724
Mohona Chakraborty <mohonachakraborty2007@gmail.com>
2026-08-26 23:54:17 +0530

    add my 13 proj

---
+++ square cube calcuator.py
@@ -0,0 +1,9 @@
+#square cube calculator 
+
+num = int(input("Enter a number : "))
+
+square= num ** 2 
+cube = num ** 3 
+
+print(" the square of the number is:", square)
+print("the cube of the number is:", cube)
